8d2c76add9098c2c40338e648c4cf11d25aa6e80
jdanton <jdanton1@yahoo.com>
2016-07-26 12:17:15 -0400

    Create sql_install.ps1

---
+++ sql_install.ps1
@@ -0,0 +1,23 @@
+& 'D:\setup.exe' /ConfigurationFile=C:\temp\ConfigurationFile.ini
+# Download Ola's scripts
+#Invoke-WebRequest -URI https://ola.hallengren.com/scripts/MaintenanceSolution.sql > C:\temp\ola.sql
+
+$CMD = 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE'
+$ARG1 = '-E'
+$ARG2 = '-i'
+$ARG3 = 'C:\temp\best_practices_run_once.sql'
+& $CMD $ARG1 $ARG2 $ARG3
+
+
+$CMD = 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE'
+$ARG1=  '-E'
+$ARG2 = '-i'
+$ARG3 = 'C:\temp\ola.sql'
+& $CMD $ARG1 $ARG2 $ARG3
+
+$CMD ='C:\Temp\SSMS-Setup-ENU.exe'
+$ARG1 =' /install'
+$ARG2 = '/norestart'
+$ARG3 = '/quiet'
+$ARG4 = '/log ssms.log'
+& $CMD $ARG1 $ARG2 $ARG3 $ARG4
